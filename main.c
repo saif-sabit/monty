220ee91b1c4c3a1a583a54abed0d1661450d2c7f
asimsharf <asimsharf@gmail.com>
2023-06-22 20:59:54 +0300

    Add initial files and headers to project

---
+++ main.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+
+    return (0);
+}
--- monty.h
+++ monty.h
@@ -1,5 +1,14 @@
 #ifndef MONTY
 #define MONTY
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -28,6 +37,5 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
 
 #endif
